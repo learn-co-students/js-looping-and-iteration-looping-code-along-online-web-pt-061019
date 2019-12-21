a67aeac9450742de790ed8dae79572ca66afd956
scrabill <shannon.crabill@gmail.com>
2019-12-20 20:44:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Code your solutions in this file
 
-function writeCards(array) {
-  for (let i = 0; 1 < array.length; i++) {
-    "Thank you, ${name}, for the wonderful birthday gift!"
+function writeCards(array, event) {
+  let new_array = []
+  for (let i = 0; i < array.length; i++) {
+    // console.log(array[i])
+    new_array.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
   }
+  return new_array
+}
 
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--)
+  }
 }

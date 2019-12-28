6559e71224b47f8adf7fa2ce2a6fe2a454672ac6
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-28 14:37:17 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"], eventName = "birthday";
+
+function writeCards(names, eventName) {
+   for (let i = 0; i < names.length; i++) {
+      console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+   }
+}
+
+function countDown(number) {
+   let i = number;
+   while(i > 0) {
+      console.log(i--);
+   }
+}
